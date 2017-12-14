b7baa2f8763a76cc3994d358f073871c9bb3df1a
vidsav <savnikvid5@gmail.com>
2017-12-14 18:29:52 +0100

    ajax finished, cleaned the design, added scroll to container

--- chat.php
+++ chat.php
@@ -1,13 +1,13 @@
 <?php
 include('db.php');
 $query = "SELECT * FROM chat ORDER BY id ASC";
-$run = $con->query($query);
+$run = $db_connect->query($query);
             
 while($row = $run->fetch_array()) :
 ?>
     <div id="chat_data">
-        <span style="color:#007bff;"><?php echo $row['name']; ?>:</span>
-        <span style="color:#BFBFBF;"><?php echo $row['msg']; ?></span>
+        <span style="color:#007bff;font-weight:bold"><?php echo $row['name']; ?>:</span>
+        <span style="color:#808080;"><?php echo $row['msg']; ?></span>
         <small style="float:right"><?php echo formatDate($row['date']); ?></small>
     </div>
-<?php endwhile; ?>
+    <?php endwhile; ?>
--- index.php
+++ index.php
@@ -1,34 +1,119 @@
 <?php include('db.php'); ?>
 
-<!DOCTYPE html>
-<html>
+    <!DOCTYPE html>
+    <html>
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>Chat</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-</head>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Chatapp</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 
-<body>
-    <h1>CHAT</h1>
-    <br>
-    <div id="container">
-        <div id="chat_box">
-        </div>
-        <form method="post"; action="">
-            <div class="form-group">
-    <input type="text" class="form-control" name="name" placeholder="Name">
-  </div>
-            <div class="form-group">
-            <textarea class="form-control" name="msg" placeholder="enter message"></textarea>
+        <script>
+            function ajax() {
+                var req = new XMLHttpRequest();
+
+                req.onreadystatechange = function () {
+                    if (req.readyState == 4 && req.status == 200) {
+                        document.getElementById('chat').innerHTML = req.responseText;
+                    }
+                }
+
+                req.open('GET', 'chat.php', true);
+                req.send();
+            }
+            setInterval(function () {
+                ajax()
+            }, 1000);
+        </script>
+        <style>
+            * {
+                padding: 0;
+                margin: 0;
+                border: 0;
+            }
+            
+            body {
+                background: url(bg.jpg) no-repeat center center fixed;
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover;
+                background-size: cover;
+            }
+            
+            h1 {
+                text-align: center;
+                color: #fff;
+            }
+            
+            #container {
+                width: 40%;
+                height: 500px;
+                background: white;
+                margin: 0 auto;
+                padding: 20px;
+                border-radius: 3px;
+                overflow-y: scroll;
+            }
+            
+            #chat_box {
+                width: 90%;
+                height: 400px;
+            }
+            
+            #chat_data {
+                width: 100%;
+                padding: 5px;
+                margin-bottom: 5px;
+                border-bottom: 1px solid silver;
+                
+            }
+            
+            .wrapper {
+                position: relative;
+                top: 0;
+                bottom: 0;
+                left: 0;
+                right: 0;
+                width: 40%;
+                height: 30%;
+                margin: auto;
+            }
+            
+            input[type='text'] {
+                width: 100%;
+                height: 40px;
+                border: 1px solid gray;
+                border-radius: 5px;
+            }
+            
+            input[type='submit'] {
+                width: 100%;
+                height: 40px;
+                border: 1px solid gray;
+                border-radius: 5px;
+            }
+            
+            textarea {
+                width: 100%;
+                height: 70px;
+                border: 1px solid gray;
+                border-radius: 5px;
+            }
+        </style>
+    </head>
+
+    <body onload="ajax();">
+        <br>
+        <h1>CHATapp</h1>
+        <br>
+        <div id="container">
+            <div id="chat_box">
+                <div id="chat"></div>
             </div>
-            <input class="btn btn-primary" type="submit" name="submit" value="Send">
-        </form>
-        <?php
+            <?php
         if(isset($_POST['submit'])){
             $name = $_POST['name'];
             $msg = $_POST['msg'];
@@ -40,7 +125,19 @@
             }
         }
         ?>
-    </div>
-</body>
-<script src="chat.js"></script>
-</html>
+        </div>
+        <br>
+        <div class="wrapper">
+            <form method="post" ; action="">
+                <div class="form-group">
+                    <input type="text" class="form-control" name="name" placeholder="Name">
+                </div>
+                <div class="form-group">
+                    <textarea class="form-control" name="msg" placeholder="Enter message"></textarea>
+                </div>
+                <input class="btn btn-primary" type="submit" name="submit" value="Send">
+            </form>
+        </div>
+    </body>
+
+    </html>
